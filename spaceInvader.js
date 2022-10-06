8ec1fec9184974bc8304da4ff4066fe1ba8cfa08
Jordy Govaerts <jordy.govaerts@student.ehb.be>
2022-10-06 10:47:56 +0200

    SpaceInvader no background

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext('2d');
+
+drawSpaceInvader();
+
+function drawSpaceInvader(){
+
+    context.beginPath();
+    context.fillStyle = "#a35ad5";
+    context.strokeStyle = "#a35ad5";
+    context.rect(100,100,50,50);
+    context.rect(150,150,50,200);
+    context.rect(100,250,50,50);
+    context.rect(200,300,50,50);
+    context.rect(200,100,50,50);
+    context.rect(250,150,50,200);
+    context.rect(300,100,50,50);
+    context.rect(300,250,50,50);
+    context.stroke();
+    context.fill();
+}
